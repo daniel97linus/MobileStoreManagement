a2f0873c38eeb8f15416079ef4a12cf30141e4c8
Daniel Linus <daniel97linus@gmail.com>
2026-05-07 15:15:06 -0500

    Initial commit

---
+++ IPG203_HW_S25/ClassUsers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPG203_HW_S25
+{
+    internal class ClassUsers 
+    {
+
+        protected   List<int> ID_Manager = new List<int> { 111 };
+        protected List<string> Name_Manager = new List<string> { "athkar" };
+        protected List<string> Password_Manager = new List<string> { "12341234" };
+
+
+        protected List<int> ID_Employee = new List<int> { 203 };
+        protected List<string> Name_Employee = new List<string> { "daniel" };
+        protected List<string> Password_Employee = new List<string> { "001122" };
+    }
+}
